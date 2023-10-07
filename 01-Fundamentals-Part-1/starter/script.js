4b42e12151d7c186645116eb0cee6e7061f062c7
vagman <bmanousakis@gmail.com>
2023-10-07 14:14:22 +0300

    inital commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,51 @@
+// LECTURE: Data Types
+const country = 'Greece';
+const continent = 'Europe';
+let population = 11000000;
+const isIsland = false;
+
+// LECTURE: let, const and var
+language = 'Greek';
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(isIsland);
+console.log(language);
+
+// LECTURE: Basic Operators
+let halfOfThePopulation = population / 2;
+console.log(halfOfThePopulation);
+console.log(population += 1);
+
+let finland_population = 6000000
+if (finland_population > population) {
+    console.log('Finland has more people than Greece.');
+} else {
+    console.log('Greece has more people than Finland.');
+}
+
+console.log(population < 33)
+
+console.log(country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language);
+
+// LECTURE: Strings and Template Literals
+
+
+// LECTURE: Taking Decisions: if / else Statements
+
+
+// LECTURE: Type Conversion and Coercion
+
+
+// LECTURE: Equality Operators: == vs. ===
+
+// LECTURE: Type Conversion and Coercion
+console.log('9' - '5'); // 4
+console.log('19' - '13' + '17'); // 617
+console.log('19' - '13' + 17); // 617
+console.log('123' < 57); // false
+console.log(5 + 6 + '4' + 9 - 4 - 2); // 1143
+
+
+
