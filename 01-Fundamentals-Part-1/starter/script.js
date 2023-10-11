798de605d2f92cf47057fc503f0e256157534a30
vagman <bmanousakis@gmail.com>
2023-10-11 11:29:06 +0300

    Completed JS Objects, Dot vs Bracket Notation

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,34 +1,34 @@
 // LECTURE: Data Types
-const country = 'Greece';
-const continent = 'Europe';
-let population = 11000000;
-const isIsland = false;
+// const country = 'Greece';
+// const continent = 'Europe';
+// let population = 11000000;
+// const isIsland = false;
 
 // LECTURE: let, const and var
-language = 'Greek';
+//language = 'Greek';
 
-console.log(country);
-console.log(continent);
-console.log(population);
-console.log(isIsland);
-console.log(language);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+// console.log(isIsland);
+// console.log(language);
 
 // LECTURE: Basic Operators
-let halfOfThePopulation = population / 2;
-console.log(halfOfThePopulation);
-console.log(population += 1);
+// let halfOfThePopulation = population / 2;
+// console.log(halfOfThePopulation);
+// console.log(population += 1);
 
-let finland_population = 6000000
-if (finland_population > population) {
-    console.log('Finland has more people than Greece.');
-} else {
-    console.log('Greece has more people than Finland.');
-}
+// let finland_population = 6000000
+// if (finland_population > population) {
+//     console.log('Finland has more people than Greece.');
+// } else {
+//     console.log('Greece has more people than Finland.');
+// }
 
-console.log(population < 33)
+// console.log(population < 33)
 
-console.log(country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language);
-
+//const description = console.log(`${country} is in  ${continent} , and its ${population} million people speak ${language}`);
+//console.log(description);
 // LECTURE: Strings and Template Literals
 
 
@@ -36,16 +36,147 @@ console.log(country + ' is in ' + continent + ', and its ' + population + ' mill
 
 
 // LECTURE: Type Conversion and Coercion
-
+'9' - '5'; // 4
+'19' - '13' + '17'; // 617
+'19' - '13' + 17; // 23
+'123' < 57; // false
+5 + 6 + '4' + 9 - 4 - 2; // 1143
 
 // LECTURE: Equality Operators: == vs. ===
 
 // LECTURE: Type Conversion and Coercion
-console.log('9' - '5'); // 4
-console.log('19' - '13' + '17'); // 617
-console.log('19' - '13' + 17); // 617
-console.log('123' < 57); // false
-console.log(5 + 6 + '4' + 9 - 4 - 2); // 1143
 
 
+// Coding Challenge #1
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+
+Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall
+*/
+const mark1Weight = 78;
+const mark2Weight = 95;
+const john1Weight = 92;
+const john2Weight = 85;
+
+const mark1Height = 1.69;
+const mark2Height = 1.88;
+const john1Height = 1.95;
+const john2Height = 1.76;
+
+const BMIJohn1 = john1Weight / john1Height ** 2;
+const BMIJohn2 = john2Weight / john2Height ** 2;
+const BMIMark1 = mark1Weight / mark1Height ** 2;
+const BMIMark2 = mark2Weight / mark2Height ** 2;
+
+console.log("Data1:\nMark's BMI = " + BMIMark1 + "\nJohn's BMI = " + BMIJohn1);
+console.log("Data2:\nMark's BMI = " + BMIMark2 + "\nJohn's BMI = " + BMIJohn2);
+
+const markHigherBMI1 = BMIMark1 > BMIJohn1;
+const markHigherBMI2 = BMIMark2 > BMIJohn2;
+
+console.log(markHigherBMI1);
+console.log(markHigherBMI2);
+
+
+// Coding Challenge #2
+/*
+Use the BMI example from Challenge #1, and the code you already wrote, and improve it:
+
+1. Print a nice output to the console, telling the user who has the higher BMI. The message can be either:
+"Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!".
+
+2. Modify the outputs above to use template literals to include the BMI values in the outputs.
+Example: "Mark's BMI (28.3) is higher than John's (23.9)!" or "John's BMI (29.1) is higher than Mark's (27)!".
+Note: Don't round the BMI values. Leave them as they are.
+*/
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+/* Write your code below. Good luck! 🙂 */
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+} else {
+    console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+}
+
+// Coding Challenge #3
+/*
+There are two gymnastics teams: Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy!
+Your tasks:
+1. Calculate the average score for each team, using the test data included below. The average score for Dolphins should be assigned to the scoreDolphins variable, and the average score of Koalas should be assigned to the scoreKoalas variable.
+2. Compare the team's average scores to determine the winner of the competition, and print to the console:
+"Dolphins win the trophy" if Dolphins win, or
+"Koalas win the trophy" if Koalas win, or
+"Both win the trophy" if their average scores are equal.
+
+TEST DATA: Dolphins scored 96, 108, and 89. Koalas scored 88, 91, and 110.
+*/
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+
+if (scoreKoalas > scoreDolphins) {
+    console.log('Koalas win the trophy');
+} else if (scoreDolphins > scoreKoalas) {
+    console.log('Dolphins win the trophy');
+} else if (scoreDolphins === scoreKoalas) {
+    console.log('Both win the trophy');
+}
+
+
+// Challenge 3.5 - Conver Switch cases to If statements
+const day = 'ssunday';
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Prepare code examples');
+} else if (day === 'friday') {
+    console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend :D')
+} else {
+    console.log('Not a valid day.')
+}
+
+/*
+CHALLENGE #4
+
+Steven needs a very simple tip calculator for whenever he goes to eat in a restaurant.
+In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+Your tasks:
+1. Calculate the tip, depending on the bill value. Create a variable called tip for this.
+It's not allowed to use an if...else statement (if it's easier for you, you can start with an if...else statement, 
+and then try to convert it to a ternary operator).
+2.Print a string to the console containing the bill value, the tip, and the final value (bill + tip).
+Example: The bill was 275, the tip was 41.25, and the total value 316.25.
+Note: Use the values of the bill and tip variables to construct this string. Don't hard-code them 🙂
+TEST DATA: Test with different bill values: 275, 40, and 430
+*/
+
+const bill = 275;
+const tip = bill <= 300 && bill >= 50 ? 0.15 * bill : 0.2 * bill;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
 
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,3 +1,93 @@
+'use strict'; // it has to be the first line of the code
 
+// let hasDriversLicence = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicence = true;
+// if (hasDriversLicence) console.log('I can drive :D');
+
+// const interface = 'Audio';
+// const private = 534;
+
+/*
+CHALLENGE #1
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
+Your tasks:
+1. Create an arrow function calcAverage to calculate the average of 3 scores. This function should have three parameters and return a single number (the average score).
+2. Create two new variables — scoreDolphins and scoreKoalas, and assign the value returned from the calcAverage function to them (you will need to call this function, and pass scores as arguments).
+3. Create a function checkWinner that takes the average score of each team as parameters (avgDolphins and avgKoalas), and then logs the winner to the console, together with the victory points, according to the rule above. Example: Koalas win (30 vs. 13) (use avgDolphins and avgKoalas instead of hard-coded values).
+4.Use the checkWinner function to determine the winner for both DATA 1 and DATA 2.
+5.Ignore draws this time. Instead, log No team wins... to the console if there is no winner.
+TEST DATA 1: Dolphins scored 44, 23, and 71. Koalas scored 65, 54, and 49.
+TEST DATA 2: Dolphins scored 85, 54, and 41. Koalas scored 23, 34, and 27
+*/
+
+// const calcAverage = (num1, num2, num3) => (num1 + num2 + num3) / 3;
+// const scoreDolphins = calcAverage(44, 23, 71);
+// const scoreKoalas = calcAverage(65, 54, 49);
+
+// const checkWinner = (avgKoalas, avgDolphins) => {
+//     if (avgDolphins * 2 === avgKoalas) {
+//         console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+//     } else if (avgKoalas * 2 === avgDolphins) {
+//         console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+//     } else {
+//         console.log(`No team wins...`);
+//     }
+// }
+
+// checkWinner(scoreKoalas, scoreDolphins);
+
+
+// const bill = 100;
+// const tip;
+
+// const calcTip = bill => {
+//     if (bill <= 300 && bill >= 50) {
+//         return tip = 0.15 * bill;
+//     else {
+//         return tip = 0.2 * bill;
+//     }
+// }
+
+/*
+CHALLENGE #2
+Steven wants you to improve his tip calculator, using the same rules as before — tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+Your tasks:
+1. Write a function calcTip that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from the first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So, create an array called bills containing the test data below.
+3. Create an array called tips containing the tip value for each bill, calculated from the function you created before.
+4. BONUS: Create an array totals containing the total values, so the bill + tip.
+TEST DATA: 125, 555, and 44.
+*/
+
+const bills = [125, 555, 44];
+
+let tip;
+const calcTip = bill => {
+    if (bill <= 300 && bill >= 50) {
+        return 0.15 * bill;
+    } else {
+        return 0.2 * bill;
+    }
+}
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+
+// Challenge at DOT vs Bracket Notation lecture:
+// Write the sentance "Jonas has 3 friends, and his best friend is called Michael." by using the object properties below
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+};
+
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}.`);
 
 
