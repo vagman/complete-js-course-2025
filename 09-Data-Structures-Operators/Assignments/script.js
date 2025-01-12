f0a38acbecbae757e259c5a1334841b897d781a9
vagman <aglaiapavlou@vagman-2.local>
2025-01-12 05:17:50 +0200

    assignments: working with strings

--- 09-Data-Structures-Operators/Assignments/script.js
+++ 09-Data-Structures-Operators/Assignments/script.js
@@ -584,3 +584,137 @@ console.log('Converted 1st book, from Books array, to Map:', firstBookMap);
 for (const [key, value] of firstBookMap) {
     if (typeof value === 'number') console.log(key);
 }
+
+console.log('----- Working with Strings - Part 1 ------');
+/*
+    15.1 Take the ISBN property of the first book from the books array, and log to the console characters at index 6, 4, 9 and 8. Use bracket notation to access individual characters.
+*/
+console.log(books[0].ISBN[6], books[0].ISBN[4], books[0].ISBN[9], books[0].ISBN[8]);
+/*
+    15.2 Below is the quote variable that stores a string. Find the index of the word 'chess', and log it to the console.
+*/
+const quote = 'A computer once beat me at chess, but it was no match for me at kick boxing';
+console.log(`The index of the word 'chess' is: ${quote.indexOf('chess')}`);
+/* 
+    15.3 Extract the word "boxing" from the same quote string, and log it to the console.
+*/
+// My solution: 
+console.log(quote.slice(-6));
+// Suggested solution:
+// console.log(quote.slice(quote.lastIndexOf(' ') + 1));
+
+/* 
+    15.4 Some authors are noted as "(Contributor)", for example "Julie Sussman (Contributor)".Create a function called isContributor that takes an author's name as an argument, and returns either true (if he's a contributor) of false(if he's not a contributor). The string "(Contributor)" is always the last part of the author's name string.
+    Example 1
+        Code: isContributor('Julie Sussman (Contributor)');
+        Expected output: true
+    Example 2:
+        Code: isContributor('Robert Sedgewick');
+        Expected output: false
+*/
+// My solution
+// const isContributor = function (name) {
+//     for (const book of books) {
+//         if (typeof book.author === 'string') {
+//             if (book.author === name) {
+//                 if (book.author.includes('(Contributor)')) {
+//                     return true;
+//                 } else {
+//                     return false;
+//                 }
+//             }
+//         } else {
+//             for (const auth of book.author) {
+//                 if (auth === name) {
+//                     if (auth.includes('(Contributor)')) {
+//                         return true;
+//                     } else {
+//                         return false;
+//                     }
+//                 }
+//             }  
+//         }
+//     }
+// };
+
+// Suggested solution:
+function isContributor(author) {
+  return author.lastIndexOf('(Contributor)') !== -1;
+}
+
+console.log(isContributor('Julie Sussman (Contributor)'));
+console.log(isContributor('Robert Sedgewick'));
+
+console.log('----- Working with Strings - Part 2 ------');
+/*
+    16.1 Write a function called normalizeAuthorName that takes an author's name (string) as an argument, and returns the same string, but the first name and last name are capitalized, and the "(Contributor)" part is removed (if exists).
+    You can be sure that the author's name always consists of two words separated by a space, and possibly ends with "(Contributor)". The string may also contain trailing spaces.
+    Example
+    Code:
+        normalizeAuthorName('  JuliE sussMan (Contributor)')
+    Expected output:
+        "Julie Sussman"
+*/
+
+
+/*
+    16.2 Take the title of the second book(books[1]) from the books array, and replace the word "Programs" with "Software".Assign the new string to the newBookTitle variable.
+*/
+
+
+
+/*
+    16.3 Write a function called logBookTheme that takes book's title (string), and logs to the console:
+    "This book is about computers" if the title starts with the word "computer",
+    "This book is about algorithms and data structures" if the title includes both the "algorithms" and "structures" words,
+    and, "This book is about some systems, but definitely not about operating systems" if the title ends with the word "system" or "systems", but doesn't include the word "operating".
+    The title may contain both small and capital letters.
+*/
+
+
+console.log('----- Working with Strings - Part 3 ------');
+/*
+    17.1 Below is the bookCategories variable that stores a string of categories.Each category is separated with a semicolon, for example, in a string "science;computing", 'science' and 'computing' are separate categories.
+    Write a function called logBookCategories that takes a string of categories separated with semicolons, and logs each category to the console(as separate strings).
+    Example
+    Code:
+        const bookCategories = 'science;computing;computer science;algorithms;business;operating systems;networking;electronics';
+        logBookCategories(bookCategories);
+    Expected output:
+        science
+        computing
+        computer science
+        algorithms
+        business
+        operating systems
+        networking
+        electronics
+*/
+
+
+/*
+    17.2 Now, the opposite.Each book from the books array has the keywords property. Write a function called getKeywordsAsString that takes the books array as an argument, collects keywords from each book, removes duplicates, and then joins them to create a single string where keywords are separated by a semicolon.
+    Example
+    Code:
+        getKeywordsAsString(books);
+    Expected output:
+        computer science; programming; algorithms;data structures; java; math; software; engineering; javascript;computer systems; C;operating systems; Java; mathematics; business; compilers; interpreters; work; focus;personal development
+*/
+
+
+/* 
+    17.3 Below is the bookChapters array that contains inner arrays.Each inner array consists of a chapter's title, and the number of a page, for example, in ['The Basics', 14], 'The Basics' is the chapter's title, and 14 is the number of a page.
+    Write a function called logBookChapters that takes an array of arrays(like bookChapters) as an argument, and logs each chapter's name to the console together with the page number. The page number should be separated from the chapter's name with underscores(take a look at the example below).
+    Use the padEnd method.
+    Example
+    Code:
+        const bookChapters = [['The Basics', 14], ['Sorting', 254], ['Searching', 372], ['Graphs', 526], ['Strings', 706]];
+        logBookChapters(bookChapters);
+    Expected output:
+        The Basics__________ 14
+        Sorting_____________ 254
+        Searching___________ 372
+        Graphs______________ 526
+        Strings_____________ 706
+*/
+
