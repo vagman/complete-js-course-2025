139ff1b28f99163d9939666bd4f1aeec0b5e1330
vagman <bmanousakis@gmail.com>
2025-02-18 15:23:22 +0200

    completed lectures 179,180

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -78,57 +78,57 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-/////////////////////////////////////////////////
-// Functions
-
+// Lecture 152: Creating DOM Elements
 const displayMovements = function (movements, sort = false) {
+  // Empty the container from fixed, hard-coded HTML withdrawals/deposits
   containerMovements.innerHTML = '';
-
+  // .textContent = 0;
+  // Lecture 170: Sorting Arrays
   const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
-
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
-
+    // Template literal containing HTML
     const html = `
-      <div class="movements__row">
-        <div class="movements__type movements__type--${type}">${
+        <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
-      </div>
-    `;
-
+          <div class="movements__date">3 days ago</div>
+          <div class="movements__value">${mov.toFixed(2)}€</div>
+        </div>`;
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
+// Lecture 160: The magic of chaining methods
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
     .map(deposit => (deposit * acc.interestRate) / 100)
     .filter((int, i, arr) => {
-      // console.log(arr);
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
+// Lecture 156: Computing usernames for the app's users
+// username should be: stw
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
     acc.username = acc.owner
@@ -138,61 +138,65 @@ const createUsernames = function (accs) {
       .join('');
   });
 };
+// We don't need to return anything because we are working on the array accounts[]. We aren't creating anything new here.
 createUsernames(accounts);
 
 const updateUI = function (acc) {
   // Display movements
   displayMovements(acc.movements);
-
-  // Display balance
+  // Dispaly balance
   calcDisplayBalance(acc);
-
   // Display summary
   calcDisplaySummary(acc);
 };
 
-///////////////////////////////////////
-// Event handlers
+// Lecture 162: The find() method
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+// console.log(account);
+
+// Exercise: convert the above functionality to a for...of loop:
+const accountForOf = function (accounts) {
+  for (const acc of accounts) {
+    if (acc.owner === 'Jessica Davis') {
+      return acc;
+    }
+  }
+};
+// console.log(accountForOf(accounts));
+// Lecture 163: Implemeting Login
+// Event Handlers
 let currentAccount;
-
 btnLogin.addEventListener('click', function (e) {
-  // Prevent form from submitting
+  // Prevent form from submiting, using and event arguement
   e.preventDefault();
-
   currentAccount = accounts.find(
     acc => acc.username === inputLoginUsername.value
   );
-  console.log(currentAccount);
-
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
-    // Display UI and message
-    labelWelcome.textContent = `Welcome back, ${
+  if (currentAccount?.pin === +inputLoginPin.value) {
+    // Display UI and a welcome message
+    labelWelcome.textContent = `Welcome back ${
       currentAccount.owner.split(' ')[0]
     }`;
     containerApp.style.opacity = 100;
-
-    // Clear input fields
+    // Clear login input fields
     inputLoginUsername.value = inputLoginPin.value = '';
-    inputLoginPin.blur();
-
-    // Update UI
+    // Remove focus on PIN element: inputLoginPin.blur(); (non needed in Chrome)
     updateUI(currentAccount);
   }
 });
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
-  inputTransferAmount.value = inputTransferTo.value = '';
-
+  inputTransferTo.value = inputTransferAmount.value = '';
   if (
     amount > 0 &&
-    receiverAcc &&
-    currentAccount.balance >= amount &&
-    receiverAcc?.username !== currentAccount.username
+    amount <= currentAccount.balance &&
+    receiverAcc?.username !== currentAccount.username &&
+    receiverAcc
   ) {
     // Doing the transfer
     currentAccount.movements.push(-amount);
@@ -203,42 +207,39 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+// Only grant a loan if there is a deposit with atleast 10% of the requested loan amount
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
-
-  const amount = Number(inputLoanAmount.value);
-
-  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+  const amount = Math.floor(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= 0.1 * amount)) {
     // Add movement
     currentAccount.movements.push(amount);
-
+    console.log(currentAccount.movements);
     // Update UI
     updateUI(currentAccount);
   }
   inputLoanAmount.value = '';
 });
 
+// Lecture 165: The findIndex() Method - Close account functionality
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
-
   if (
-    inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    currentAccount.username === inputCloseUsername.value &&
+    currentAccount.pin === +inputClosePin.value
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
     );
-    console.log(index);
-    // .indexOf(23)
-
-    // Delete account
+    // Delete account: Splice works on the array itself and doesn't create a new one
     accounts.splice(index, 1);
+
+    // Clear fields
+    inputCloseUsername.value = inputClosePin.value = '';
 
     // Hide UI
     containerApp.style.opacity = 0;
   }
-
-  inputCloseUsername.value = inputClosePin.value = '';
 });
 
 let sorted = false;
@@ -248,6 +249,99 @@ btnSort.addEventListener('click', function (e) {
   sorted = !sorted;
 });
 
+// Lecture 171: Array Grouping
+const groupedAccounts = Object.groupBy(accounts, ({ type }) => type);
+console.log(groupedAccounts);
+
+// Let's assume we do not have the movements stores in an array. They are only displayed in the UI.
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    element => +element.textContent.replace('€', '')
+  );
+  console.log(movementsUI);
+  movementsUI2 = [...document.querySelectorAll('.movements__value')];
+  // Here mapping has to be done seperatly so Array.from() is cleaner
+});
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+// Lecture 179: Converting and Checking Numbers
+console.log(23 === 23.0); // true
+
+// Decimal (base 10): 0-9
+// Binary (base 2): 0-1
+console.log(0.1 + 0.2); // 0.30000000000000004
+console.log(0.1 + 0.2 === 0.3);
+
+// Conversion
+console.log(Number('23'));
+console.log(+'23');
+
+// Parsing
+console.log(Number.parseInt('30px'), 10); // 30 , base 10
+console.log(Number.parseInt('e30'), 10); // NaN , number has to be at the beginning
+console.log(Number.parseFloat('   2.5rem')); // 2.5
+console.log(Number.parseInt('   2.5rem   ')); // 2
+
+// check if a value is NaN
+console.log(Number.isNaN(20)); // false
+console.log(Number.isNaN('20')); // false
+console.log(Number.isNaN(+'20X')); // true
+console.log(Number.isNaN(23 / 0)); // false , Infinity value type
+
+// Best way of checking if a value is number
+console.log(Number.isFinite(20)); // true
+console.log(Number.isFinite('20')); // false
+console.log(Number.isFinite(+'20X')); // false
+console.log(Number.isFinite(23 / 0)); // false
+
+console.log(Number.isInteger(23)); // true
+console.log(Number.isInteger(23.0)); // true
+console.log(Number.isInteger(23 / 0)); // false
+
+// Lecture 180: Math and Rounding
+console.log(Math.sqrt(25)); // swuare root
+console.log(25 ** (1 / 2));
+console.log(25 ** (1 / 3)); // cubic root
+
+console.log(Math.max(5, 18, 23, 11, 2));
+console.log(Math.max(5, 18, '23', 11, 2));
+console.log(Math.max(5, 18, '23px', 11, 2));
+
+console.log(Math.min(5, 18, 23, 11, 2));
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+// Random values 1 - 6
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+console.log(randomInt(10, 20));
+console.log(randomInt(0, 3));
+
+// Rounding integers
+console.log(Math.trunc(23.3));
+
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
+
+// Round up
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(23.9));
+
+// Round down
+console.log(Math.floor(23.9));
+console.log(Math.floor('23.9'));
+
+console.log(Math.trunc(-23.3));
+console.log(Math.floor(-23.3));
+
+// Rounding decimals
+console.log((2.7).toFixed(0)); // returns String !!!
+console.log((2.7).toFixed(3));
+console.log((2.345).toFixed(2));
+console.log(+(2.345).toFixed(2)); // Convert it back to Number
