13cf8d36f18cd250de2f0cdda53e158a417b05fd
vagman <bmanousakis@gmail.com>
2025-02-25 01:28:01 +0200

    completed smooth scrolling

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -115,3 +115,37 @@ logo.classList.contains('c'); // Be careful: X .includes()
 
 // DO NOT use this - It will override all the existing classes
 logo.className = 'Jonas';
+// Lecture 198: Implementing Smooth Scrolling
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  // x (top), y (left) = distance of the element from the left and top of the browser window
+  console.log(e.target.getBoundingClientRect());
+  // Deprecation:
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.scrollX,
+  //   s1coords.top + window.scrollY
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.scrollX,
+  //   top: s1coords.top + window.scrollY,
+  //   behavior: 'smooth',
+  // });
+
+  // More modern way
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
