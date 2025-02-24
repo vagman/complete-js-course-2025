9ef8930c80750222979fede5d20d0cb26aa73e84
vagman <bmanousakis@gmail.com>
2025-02-24 20:14:01 +0200

    Completed lecture 196: Selecting, Creating, and Deleting Elements

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -1,8 +1,6 @@
 'use strict';
 
-///////////////////////////////////////
 // Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
@@ -29,3 +27,43 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// Lecture 196: Selecting, Creating, and Deleting Elements
+// Selecting Elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+
+console.log(allButtons); // Returns a HTMLcollection
+
+console.log(document.getElementsByClassName('btn'));
+
+// Creating & Inserting Elements
+// .insertAdjacentHTML --> Bankist app Section 12
+const message = document.createElement('div');
+message.classList.add('cookie-message'); // CSS Class from style.css
+// message.textContent = 'We use cookies for improved functionality and analytics';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics <button class="btn btn--close-cookie">Got it!</button>';
+
+//header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+// header.before(message);
+// header.after(message);
+
+// Deleting Elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+    // Old way of deleting elements
+    // message.parentElement.removeChild(message);
+  });
