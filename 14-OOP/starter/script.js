eeadf06b0067a84e3b6afd89845f3ca5668f0ce6
vagman <bmanousakis@gmail.com>
2025-03-17 15:40:47 +0200

    prototypal inheritance on built-in objects

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -53,3 +53,42 @@ console.log(vagman, matilda);
 
 console.log(vagman.hasOwnProperty('firstName')); // true
 console.log(vagman.hasOwnProperty('species')); // false, it isn't implemented in the original object but inherited from the prototype property
+
+// Lectu§ 222: Prototypal Inheritance on Built-in Objects
+// Deprecated __proto__ property
+console.log(vagman.__proto__);
+
+// New property used in modern JS
+console.log(Object.getPrototypeOf(vagman));
+
+console.log(vagman.__proto__.__proto__); // Top of prototype chain: Object.prototype
+console.log(vagman.__proto__.__proto__.__proto__); // null
+
+console.log(Person.prototype.constructor);
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 6, 5, 6, 9, 9];
+console.log(arr.__proto__); // Array
+console.log(Array.prototype); // Array
+console.log(arr.__proto__ === Array.prototype); // true
+
+console.log(arr.__proto__.__proto__); // Object
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(h1); // [[Prototype]]: HTMLHeadingElement
+console.dir(h1.__proto__); // HTMLHeadingElement
+console.dir(h1.__proto__.__proto__); // HTMLElement
+console.dir(h1.__proto__.__proto__.__proto__); // Element
+console.dir(h1.__proto__.__proto__.__proto__.__proto__); // Node
+console.dir(h1.__proto__.__proto__.__proto__.__proto__.__proto__); // EventTarget
+console.dir(h1.__proto__.__proto__.__proto__.__proto__.__proto__.__proto__); // Object
+console.dir(
+  h1.__proto__.__proto__.__proto__.__proto__.__proto__.__proto__.__proto__
+); // null - end of prototype chain
+
+console.dir(x => x + 1);
