1a41853b0b2faadcb1f66b8e6b2d4cfcaf5eab00
vagman <bmanousakis@gmail.com>
2025-03-29 18:13:12 +0200

    initialized mapty project letures 240-246

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,6 +10,7 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+let map, mapEvent;
 
 navigator.geolocation.getCurrentPosition(
   function (position) {
@@ -17,8 +18,63 @@ navigator.geolocation.getCurrentPosition(
     const { longitude } = position.coords;
     console.log(latitude, longitude);
     console.log(`https://www.google.com/maps/@${latitude},${longitude},15z`);
+
+    const coords = [latitude, longitude];
+    map = L.map('map').setView(coords, 13);
+
+    // console.log(map);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    // Handling clicks on map
+    map.on('click', function (mapE) {
+      mapEvent = mapE;
+      form.classList.remove('hidden');
+      inputDistance.focus();
+    });
   },
   function () {
     alert('Could not get your location.');
   }
 );
+
+form.addEventListener('submit', function (e) {
+  // Display market
+  e.preventDefault();
+
+  // Clear input fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+
+  const { lat, lng } = mapEvent.latlng;
+  const markerOptions = {
+    riseOnHover: true,
+    opacity: 0.8,
+  };
+
+  const popupOptions = {
+    maxWidth: 250,
+    minWidth: 100,
+    autoClose: false,
+    closeOnClick: false,
+    className: 'running-popup',
+  };
+
+  L.marker([lat, lng], markerOptions)
+    .addTo(map)
+    .bindPopup(L.popup(popupOptions))
+    .setPopupContent('Workout')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  // https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
